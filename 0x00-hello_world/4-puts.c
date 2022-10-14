7c80f895edac799bcd2dd1b3d7ffde13e5318cab
root <julietanine.com>
2022-10-13 20:10:11 -0700

    my new tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-#!/bin/bash
-
 #include <stdio.h>
-/**
- *  * Description: main - Prints out a string to stdout.
- *   * Return: 0 if success.
- *    */
-int main(void)
+ /**
+ * main -Entry
+ * Return: Always 0
+ * /
+ * init main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
