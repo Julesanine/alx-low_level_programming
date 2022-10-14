3cca92e3aa179197e7a0d79564cb33b04d60607c
root <julietanine.com>
2022-10-14 05:02:11 -0700

    my new tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-
 #include <stdio.h>
- /**
- * main -Entry
- * Return: Always 0
- * /
- * init main(void)
+/**
+ *  *main- Entry point for the program.
+ *  *Return: By default returns zero if no erros.
+*/
+
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
