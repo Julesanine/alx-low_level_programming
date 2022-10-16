1753d31a8f140561b728d4b086bf6a57cdb989f0
Julesanine <113605696+Julesanine@users.noreply.github.com>
2022-10-16 12:50:24 +0100

    Update 4-puts.c
    my new tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 /**
-*Description main -Entry point for program. 
-*Return: 0 if success. 
-*/
-init main(void)
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
 {
-	puts("\"Programming is build like a multilingual puzzle");
-	return (0)
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
